5d4367f40200ebdebbbd7242b9578421ac676e68
Chirasree <chirasbanikagt2000@gmail.com>
2024-04-22 22:15:25 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+var swiper = new Swiper(".product-slider", {
+  loop:true,
+  spaceBetween: 20,
+  autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+    1220: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+function addToCart() {
+  //const cartCountSpan = document.querySelector('.btn success');
+  let currentCount = 0;
+  currentCount++;
+  
+  console.log(currentCount)
+}
